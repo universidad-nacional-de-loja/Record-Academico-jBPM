49dcd83de64e51d438426f821b43076a5299361d
wbadmin <>
2023-05-16 12:58:50 -0500

    {/src/main/java/com/espacio_prueba_jbpm/recordacademico2/Marciano.java}

--- src/main/java/com/espacio_prueba_jbpm/recordacademico2/Marciano.java
+++ src/main/java/com/espacio_prueba_jbpm/recordacademico2/Marciano.java
@@ -7,29 +7,38 @@ package com.espacio_prueba_jbpm.recordacademico2;
 @javax.persistence.Entity
 public class Marciano implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "MARCIANO_ID_GENERATOR")
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(name = "MARCIANO_ID_GENERATOR", sequenceName = "MARCIANO_ID_SEQ")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "MARCIANO_ID_GENERATOR")
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(name = "MARCIANO_ID_GENERATOR", sequenceName = "MARCIANO_ID_SEQ")
+	private java.lang.Long id;
 
-    public Marciano() {
-    }
-    
-    public Marciano(java.lang.Long id) {
-        this.id = id;
-    }
+	@org.kie.api.definition.type.Label(value = "Nombre")
+	private java.lang.String name;
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public Marciano() {
+	}
 
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public Marciano(java.lang.Long id, java.lang.String name) {
+		this.id = id;
+		this.name = name;
+	}
 
 }
